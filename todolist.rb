e8a05d4937ad7429dddf377cc2a00d2a9359193f
Tyler <tyler@tylermorgan.me>
2016-01-02 20:12:54 -0800

    Added List.completed?, Item priorities, and List.rename_item

--- todolist.rb
+++ todolist.rb
@@ -4,21 +4,45 @@ class TodoList
     @title = list_title
     @items = Array.new # Starts empty! No Items yet!
   end
+
   attr_accessor :title, :items
-  def add_item(new_item)
-    item = Item.new(new_item)
+
+  def add_item(new_item, priority=0)
+    item = Item.new(new_item,priority)
     @items.push(item)
   end
-  def print_list
-    puts "-" * @title.length
-    puts @title
-    puts "-" * @title.length
+
+  def rename(list_title)
+    @title = list_title
+  end
+
+  def delete_item(index)
+    @items.delete_at(index)
+  end
+
+  def toggle_status(index)
+    @items[index].toggle_status
+  end
+
+  def rename_item(index, description)
+    @items[index].description = description
+  end
+
+  def completed?
+    return @items.all? {|item| item.completion_status == true}
+  end
+
+  def print
+    header = "#{@title} -- Completed: #{completed?}"
+    puts "-" * header.length
+    puts header
+    puts "-" * header.length
     longest_word = @items.inject(0) do |previous_length, current_word|
       current_length = current_word.description.length
       current_length > previous_length ? current_length : previous_length
     end
     @items.each_index {|index|
-      puts "#{index} - #{@items[index].description}".ljust(longest_word + 7) + "Completed: #{@items[index].completed_status}"
+      puts "#{index} - [#{items[index].priority}]#{@items[index].description}".ljust(longest_word + 10) + "Completed: #{@items[index].completion_status}"
     }
   end
 end
@@ -26,11 +50,20 @@ end
 class Item
   # Initialize item with a description and marked as
   # not complete
-  def initialize(item_description)
+  def initialize(item_description, priority=0)
      @description = item_description
-     @completed_status = false
+     @completion_status = false
+     @priority = priority
   end
 
   # methods and stuff go here
-  attr_accessor :description, :completed_status
+  attr_accessor :description, :completion_status, :priority
+
+  def toggle_status
+    @completion_status = !@completion_status
+  end
+
+  def print
+    puts "#{@description} Completed: #{@completion_status} Priority: #{@priority}"
+  end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -10,21 +10,54 @@ todo.add_item("Write Code")
 todo.add_item("Take Over the World")
 
 # Print the list
-
-todo.print_list
+todo.print
 
 # Delete the first item
+todo.delete_item(0)
 
 # Print the list
+todo.print
 
 # Delete the second item
+todo.delete_item(1)
 
 # Print the list
+todo.print
 
 # Update the completion status of the first item to complete
+todo.toggle_status(0)
 
 # Print the list
+todo.print
 
 # Update the title of the list
+todo.rename("A New Named List")
 
 # Print the list
+todo.print
+
+###################
+# Additional output
+###################
+
+#Test Item.print
+todo.items[0].print
+
+# Add an item with priority
+todo.add_item("Get Milk", 1)
+
+# Print the list
+todo.print
+
+# Rename the last item
+todo.rename_item(2, "Drink Mega Milk")
+
+# Print the list
+todo.print
+
+# Complete all items
+todo.toggle_status(1)
+todo.toggle_status(2)
+
+# Print the list
+todo.print
